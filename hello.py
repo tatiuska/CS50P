4c18912e5da5612569e2e3b42959635ea185a272
tatiuska <tfweska@gmail.com>
2024-07-30 15:56:54 -0300

    format string as a final solution to the program hello.py

--- hello.py
+++ hello.py
@@ -1,1 +1,18 @@
-print("hello, world!")
+# Ask user for their name
+name = input("What's your name? ")
+
+# Saying hello to user
+# print("hello, ")
+# print(name)
+# print("hello, " + name)
+# print("hello,", name)
+# print("hello, ", end="")
+# # print(name)
+# print("hello,", name, sep="")
+
+# How to print quotes
+# print('hello, "friend"')
+# print("hello, \"friend\"")
+
+# print("hello, {name}")
+print(f"hello, {name}")

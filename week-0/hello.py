07d59783383b7bec385ea533a22e8e99e7155513
tatiuska <tfweska@gmail.com>
2024-08-04 16:17:50 -0300

    segunda iteração da função hello(), com parâmetro to, que receberá o input da variável name quando a função for chamada

--- week-0/hello.py
+++ week-0/hello.py
@@ -1,11 +1,11 @@
 # Creating the function hello
-def hello():
-    print("hello")
+def hello(to):
+
+    print("hello,", to)
 
 # Ask user for their name
 name = input("What's your name? ")
-hello()
-print(name)
+hello(name)
 
 # # Ask user for their name
 # name = input("What's your name? ").strip().title()

58e4d5608506e2d67709c4856369703e6c565b7d
tatiuska <tfweska@gmail.com>
2024-08-04 13:05:12 -0300

    mudando o tipo de dado de int para float na calculadora

--- week-0/calculator.py
+++ week-0/calculator.py
@@ -1,6 +1,6 @@
 # Variables to start the calculator
-x = int(input("What's x? "))
-y = int(input("What's y? "))
+x = float(input("What's x? "))
+y = float(input("What's y? "))
 
 # Printing the results
 print(x + y)

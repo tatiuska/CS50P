021bc5fac565fbf7d0f848704d6c5ab656c2b86f
tatiuska <tfweska@gmail.com>
2024-08-29 19:39:40 -0300

    segunda iteração do programa usando if e elif

--- week-1/compare.py
+++ week-1/compare.py
@@ -5,9 +5,9 @@ y = int(input("What's y? "))
 if x < y:
     print("x is less than y.")
 
-if x > y:
+elif x > y:
     print("x is greater than y.")
 
-if x == y:
+elif x == y:
     print("x is equal to y.")
 

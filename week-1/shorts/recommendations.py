c7d2df23a98619829e329445ac06e4a3f9e75191
tatiuska <tfweska@gmail.com>
2024-08-30 18:39:14 -0300

    reescrevendo o programa recommendations.py com expressões booleanas e operadores booleanos.

--- week-1/shorts/recommendations.py
+++ week-1/shorts/recommendations.py
@@ -2,27 +2,26 @@
 
 def main():
     difficulty = input("Difficult or Casual? ")
-    players = input("Multiplayer or Single-player? ")
-    
-    if difficulty == "Difficult":
-        if players == "Multiplayer":
-            recommend("Poker")
-        elif players == "Single-player":
-            recommend("Klondike")
-        else:
-            print("Enter a valid number of players")
+    if not (difficulty == "Difficult" or difficulty == "Casual"):
+        print("Enter a valid difficulty")
+        return
 
-    elif difficulty == "Casual":
-        if players == "Multiplayer":
-            recommend("Hearts")
-        elif players == "Single-player":
-            recommend("Clock")
-        else:
-            print("Enter a valid number of players")  
+    players = input("Multiplayer or Single-player? ")
+    if not (players == "Multiplayer" or players == "Single-player"):
+        print("Enter a valid number of players")
+        return
+    
+    if difficulty == "Difficult" and players == "Multiplayer":
+        recommend("Poker")
+
+    elif difficulty == "Difficult" and players == "Single-player":
+        recommend("Klondike")
+    
+    elif difficulty == "Casual" and players == "Multiplayer":
+        recommend("Hearts")
 
     else:
-        print("Enter a valid difficulty")
-  
+        recommend("Clock")
 
 
 def recommend(game):

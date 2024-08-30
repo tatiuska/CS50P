75e5952f0ea380d6d6ae451b1882b4c397ecb4ca
tatiuska <tfweska@gmail.com>
2024-08-30 16:22:59 -0300

    terceira iteração do programa house.py com uso do match/case

--- week-1/house.py
+++ week-1/house.py
@@ -1,9 +1,16 @@
 # program that tells which HP house you belong to
 name = input("What's your name? ")
 
-if name == "Harry" or name == "Hermione" or name == "Ron":
-    print("Gryffindor")
-elif name == "Draco":
-    print("Slytherin")
-else:
-    print("Who?")
+match name:
+    case "Harry":
+        print("Gryffindor")
+    case "Hermione":
+        print("Gryffindor")
+    case "Ron":
+        print("Gryffindor")
+    case "Draco":
+        print("Slytherin")
+    # sintaxe para qualquer outra possibilidade não contemplada pelo match/case
+    case _:
+        print("Who?")    
+

687b3c943bfcbc2d77f40b2113c70670febd95ab
tatiuska <tfweska@gmail.com>
2024-08-30 16:01:01 -0300

    nova iteração do programa parity.py com criação de funções main() e is_even().

--- week-1/parity.py
+++ week-1/parity.py
@@ -1,9 +1,19 @@
 # program to tell if a number is even or odd.
-x = int(input("What's x? "))
+def main():
+    x = int(input("What's x? "))
+    if is_even(x):
+        print("Even")
+    
+    else:
+        print("Odd")
 
-if x % 2 == 0:
-    print("Even")
 
-else:
-    print("Odd")
+def is_even(n):
+    if n % 2 == 0:
+        return True
+    else:
+        return False
+
+
+main()
 

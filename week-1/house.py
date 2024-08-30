706235836d2d6f5c28cc687af21abed19ae81043
tatiuska <tfweska@gmail.com>
2024-08-30 16:15:46 -0300

    primeira iteração do programa house.py

---
+++ week-1/house.py
@@ -0,0 +1,13 @@
+# program that tells which HP house you belong to
+name = input("What's your name? ")
+
+if name == "Harry":
+    print("Gryffindor")
+elif name == "Hermione":
+    print("Gryffindor")
+elif name == "Ron":
+    print("Gryffindor")
+elif name == "Draco":
+    print("Slytherin")
+else:
+    print("Who?")

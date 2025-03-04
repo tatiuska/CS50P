0c51b05e5e645eb14540ecb1b34bc32d964c18fd
tatiuska <tfweska@gmail.com>
2025-03-04 18:42:51 -0300

    criando uma função meow(), versão com hardcode

--- week-2/cat.py
+++ week-2/cat.py
@@ -1,22 +1,34 @@
 #funciton to demonstrate how loops works
-#using while
+#01 - using while
 #i = 0
 #while i < 3:
 #    print("meow")
 #    i += 1
 
-#using for
+#02 - using for
 #for _ in range(8):
 #    print("meow")
 
-#version using only print
+#03 - version using only print
 #print("meow\n" * 3, end="")
 
-#using while true
-while True:
-    n = int(input("What's n? "))
-    if n > 0:
-        break
+#04 - using while true
+#while True:
+#    n = int(input("What's n? "))
+#    if n > 0:
+#        break
 
-for _ in range(n):
-    print("meow")
+#for _ in range(n):
+#    print("meow")
+
+#05 - creating a meow function - hardcode version
+def main():
+    meow(3)
+
+
+def meow(n):
+    for _ in range(n):
+        print("meow")
+
+
+main()

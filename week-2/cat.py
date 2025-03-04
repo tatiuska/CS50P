9fdd88acbeab299591b0cff49aa61ecee05946ed
tatiuska <tfweska@gmail.com>
2025-03-04 18:21:20 -0300

    versão do programa usando for

--- week-2/cat.py
+++ week-2/cat.py
@@ -1,5 +1,10 @@
 #funciton to demonstrate how loops works
-i = 0
-while i < 3:
+#using while
+#i = 0
+#while i < 3:
+#    print("meow")
+#    i += 1
+
+#using for
+for i in [0, 1, 2]:
     print("meow")
-    i += 1

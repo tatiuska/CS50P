932ef43d8be78bf8092ffef79f80292dbb9ca28a
tatiuska <tfweska@gmail.com>
2025-03-05 20:17:32 -0300

    versão com dicionário e for

--- week-2/hogwarts.py
+++ week-2/hogwarts.py
@@ -1,5 +1,30 @@
 # working with lists
-students = ["Hermione", "Harry", "Ron"]
+# students = ["Hermione", "Harry", "Ron", "Draco"]
+# houses = ["Gryffindor", "Gryffindor", "Gryffindor", "Slytherin"]
 
-for i in range(len(students)):
-    print(i + 1, students[i])
+# for i in range(len(students)):
+#     print(i + 1, students[i])
+
+# working with dictionaries
+# students = {
+#     "Hermione": "Gryffindor",
+#     "Harry": "Gryffindor",
+#     "Ron": "Gryffindor",
+#     "Draco": "Slytherin",
+# }
+
+# print(students["Hermione"])
+# print(students["Harry"])
+# print(students["Ron"])
+# print(students["Draco"])
+
+# escalating 
+students = {
+    "Hermione": "Gryffindor",
+    "Harry": "Gryffindor",
+    "Ron": "Gryffindor",
+    "Draco": "Slytherin",
+}
+
+for student in students:
+    print(student, students[student], sep=", ")

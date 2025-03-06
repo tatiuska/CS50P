bb2a42815eb387737b8130f488a34b2f2a000520
tatiuska <tfweska@gmail.com>
2025-03-06 15:01:26 -0300

    sexta versão mario.py com função print_square()

--- week-2/mario.py
+++ week-2/mario.py
@@ -1,9 +1,15 @@
 def main():
-    print_row(4)
+    print_square(3)
 
 
-def print_row(width):
-    print("?" * width)
+def print_square(size):
+    # for each row in square
+    for i in range(size):
+        #for each brick in row
+        for j in range(size):
+            #print brick
+            print("#", end="")
+        print()
 
     
 main()

245d4ac968046277ee6acd9153ef80cbdf4fc537
tatiuska <tfweska@gmail.com>
2025-03-06 14:36:29 -0300

    versão final com mais dados, usando listas e dicionários

--- week-2/hogwarts.py
+++ week-2/hogwarts.py
@@ -19,12 +19,23 @@
 # print(students["Draco"])
 
 # escalating 
-students = {
-    "Hermione": "Gryffindor",
-    "Harry": "Gryffindor",
-    "Ron": "Gryffindor",
-    "Draco": "Slytherin",
-}
+# students = {
+#     "Hermione": "Gryffindor",
+#     "Harry": "Gryffindor",
+#     "Ron": "Gryffindor",
+#     "Draco": "Slytherin",
+# }
+
+# for student in students:
+#     print(student, students[student], sep=", ")
+
+# working with more data
+students = [
+    {"name": "Hermione", "house": "Gryffindor", "patronus": "Otter"},
+    {"name": "Harry", "house": "Gryffindor", "patronus": "Stag"},
+    {"name": "Ron", "house": "Gryffindor", "patronus": "Jack Russel terrier"},
+    {"name": "Draco", "house": "Slytherin", "patronus": None}
+]
 
 for student in students:
-    print(student, students[student], sep=", ")
+    print(student["name"], student["patronus"], student["house"], sep=", ")
